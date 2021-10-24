58752a17d5f2867c7dcf676cee626dec395e7fea
Pavel Valentov <pavel@valentov.com>
2021-10-24 16:14:53 +0300

    added 4 methods

--- index.js
+++ index.js
@@ -1,63 +1,112 @@
 class PrettyNumbers {
-    constructor() {
+  constructor() {
 
+  }
+
+  static isNumberMirrored(n) {
+    let s = n.toString();
+
+    if (s.length < 3) return false;
+
+    let mirrored = true;
+    const mid = Math.floor(s.length / 2);
+    for (let m = 0; m < mid; m++) {
+      if (s[m] !== s[s.length - m - 1]) {
+        mirrored = false;
+        break;
+      }
     }
 
-    static isNumberMirrored(n) {
-        let s = n.toString();
+    return mirrored;
+  }
 
-        if (s.length < 3) return false;
+  static isOneDigitNumber(n) {
+    let s = n.toString();
 
-        let mirrored = true;
-        const mid = Math.floor(s.length / 2);
-        for (let m = 0; m < mid; m++) {
-            if (s[m] !== s[s.length - m - 1]) {
-                mirrored = false;
-                break;
-            }
-        }
-
-        return mirrored;
+    let oneNumbered = true;
+    for (let m = 0; m < s.length - 1; m++) {
+      if (s[m] !== s[m + 1]) {
+        oneNumbered = false;
+        break;
+      }
     }
 
-    static isOneDigitNumber(n) {
-        let s = n.toString();
+    return oneNumbered;
+  }
 
-        let oneNumbered = true;
-        for (let m = 0; m < s.length - 1; m++) {
-            if (s[m] !== s[m + 1]) {
-                oneNumbered = false;
-                break;
-            }
-        }
+  static getPairsCountInNumber(n) {
+    let s = n.toString();
+    let pairs = 0;
 
-        return oneNumbered;
+    for (let m = 0; m < s.length - 1; m++) {
+      if (s[m] === s[m + 1]) {
+        pairs++;
+        m++;
+      }
     }
 
-    static getPairsCountInNumber(n) {
-        let s = n.toString();
-        let pairs = 0;
+    return pairs;
+  }
 
-        let oneNumbered = true;
-        for (let m = 0; m < s.length - 1; m++) {
-            if (s[m] === s[m + 1]) {
-                pairs++;
-                m++;
-            }
-        }
+  static getTriplesCountInNumber(n) {
+    let s = n.toString();
+    let triples = 0;
 
-        return pairs;
+    for (let m = 0; m < s.length - 2; m++) {
+      if (s[m] === s[m + 1] && s[m] === s[m + 2] ) {
+        triples++;
+        m += 2;
+      }
     }
+
+    return triples;
+  }
 }
 
-for (let i = 100000; i < 200000; i++) {
-    let s = i.toString();
+console.log('number;mirrored;one-digit;pairs;triples');
 
-    PrettyNumbers.isNumberMirrored(i) ? s += ';1' : s += ';0';
+let count = 0;
+let s;
+let isPremium;
 
-    PrettyNumbers.isOneDigitNumber(i) ? s += ';1' : s += ';0';
+for (let i = 0; i < 1000000; i++) {
+  s = i.toString();
+  isPremium = false;
 
+  if (PrettyNumbers.isNumberMirrored(i)) {
+    isPremium = true;
+    s += ';1';
+  } else {
+    s += ';0';
+  }
+
+  if (PrettyNumbers.isOneDigitNumber(i)) {
+    isPremium = true;
+    s += ';1';
+  } else {
+    s += ';0';
+  }
+
+  if (PrettyNumbers.getPairsCountInNumber(i) > 2) {
+    isPremium = true;
     s += ';' + PrettyNumbers.getPairsCountInNumber(i).toString();
+  } else {
+    s += ';0';
+  }
 
+  if (PrettyNumbers.getTriplesCountInNumber(i) > 0) {
+    isPremium = true;
+    s += ';' + PrettyNumbers.getTriplesCountInNumber(i).toString();
+  } else {
+    s += ';0';
+  }
+
+  if (isPremium) {
     console.log(s);
+
+    count++;
+  }
+
 }
+
+console.log('***', count.toString());
